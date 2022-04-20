f197459f1f98a614db5ad578aab78fddc4633b4c
Andi Faizal <andifaizal647@gmail.com>
2022-04-21 05:05:51 +0700

    adjust cta

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -5,13 +5,12 @@ import { HiArrowNarrowRight } from "react-icons/hi";
 const Home = () => {
   return (
     <div name="home" className="w-full h-screen bg-[#0a192f]">
-      {/* Container */}
       <div className="max-w-[1000px] mx-auto px-8 flex flex-col justify-center h-full">
         <p className="text-green-500 text-xl font-semibold">Hi, my name is</p>
-        <h1 className="text-4xl sm:text-7xl font-bold text-[#ccd6f6]">
+        <h1 className="text-2xl sm:text-7xl font-bold text-[#ccd6f6]">
           Andi Faizal
         </h1>
-        <h2 className="text-4xl sm:text-7xl font-bold text-[#8892b0]">
+        <h2 className="text-2xl sm:text-7xl font-bold text-[#8892b0]">
           I'm a Full Stack Developer.
         </h2>
         <p className="text-[#8892b0] py-4 max-w-[700px]">
@@ -20,18 +19,24 @@ const Home = () => {
           building responsive full-stack web applications.
         </p>
         <div className="flex">
-          <button className="text-white group border-2 px-6 py-3 mx-2 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
-            <Link to="/portofolio">View Work</Link>
+          <Link
+            to="/portofolio"
+            className="text-white group border-2 px-6 py-3 mx-2 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600"
+          >
+            View Work
             <span className="group-hover:rotate-90 duration-300">
               <HiArrowNarrowRight className="ml-3 " />
             </span>
-          </button>
-          <button className="text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600">
-            <Link to="/contact">Contact </Link>
+          </Link>
+          <Link
+            to="/contact"
+            className=" text-white group border-2 px-6 py-3 my-2 flex items-center hover:bg-pink-600 hover:border-pink-600"
+          >
+            Contact
             <span className="group-hover:rotate-90 duration-300">
               <HiArrowNarrowRight className="ml-3 " />
             </span>
-          </button>
+          </Link>
         </div>
       </div>
     </div>
