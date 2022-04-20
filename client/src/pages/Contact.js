800930a937c06a0eab071329c62238dfc6d29a9f
Andi Faizal <andifaizal647@gmail.com>
2022-04-21 02:53:41 +0700

    add content in pages

--- client/src/pages/Contact.js
+++ client/src/pages/Contact.js
@@ -1,6 +1,17 @@
 import React from "react";
 
 function Contact() {
-  return <div>Contact</div>;
+  return (
+    <div className="w-full h-screen pt-24">
+      <div className="max-w-[1000px] mx-auto  h-full">
+        <p className="p-4 max-w-[700px]">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga est
+          ullam aperiam neque ex, quam, deserunt, ducimus molestias magni
+          voluptate nostrum eligendi nihil. Rem provident quasi consectetur
+          numquam dolores pariatur.
+        </p>
+      </div>
+    </div>
+  );
 }
 export default Contact;
--- client/src/pages/Portofolio.js
+++ client/src/pages/Portofolio.js
@@ -1,7 +1,18 @@
 import React from "react";
 
 function Portofolio() {
-  return <div>Portofolio</div>;
+  return (
+    <div className="w-full h-screen pt-24">
+      <div className="max-w-[1000px] mx-auto flex flex-col h-full">
+        <p className="p-4 max-w-[700px]">
+          Lorem ipsum dolor, sit amet consectetur adipisicing elit. Fuga est
+          ullam aperiam neque ex, quam, deserunt, ducimus molestias magni
+          voluptate nostrum eligendi nihil. Rem provident quasi consectetur
+          numquam dolores pariatur.
+        </p>
+      </div>
+    </div>
+  );
 }
 
 export default Portofolio;
